bd7ba88dcddbf6938f904acfc0c13c86b708c1ac
antonycorp11-blip <antonycorp11@gmail.com>
2026-01-16 23:39:57 -0400

    feat: unique images for first 4 cards and directory structure for rest

--- constants/cards.ts
+++ constants/cards.ts
@@ -2,11 +2,11 @@
 import { Card } from '../types';
 
 export const CARDS: Card[] = [
-    // COMUM - 5,000 XP
-    { id: 'c1', name: 'Blue Rhythm', rarity: 'comum', price: 0, image: 'url(/assets/cards/common.png)' },
-    { id: 'c2', name: 'Emerald Deck', rarity: 'comum', price: 0, image: 'url(/assets/cards/common.png)' },
-    { id: 'c3', name: 'Slate Echo', rarity: 'comum', price: 0, image: 'url(/assets/cards/common.png)' },
-    { id: 'c4', name: 'Teal Pulse', rarity: 'comum', price: 0, image: 'url(/assets/cards/common.png)' },
+    // COMUM - 5,000 XP (Artes Únicas Liberadas)
+    { id: 'c1', name: 'Blue Rhythm', rarity: 'comum', price: 0, image: 'url(/assets/cards/c1.png)' },
+    { id: 'c2', name: 'Emerald Deck', rarity: 'comum', price: 0, image: 'url(/assets/cards/c2.png)' },
+    { id: 'c3', name: 'Slate Echo', rarity: 'comum', price: 0, image: 'url(/assets/cards/c3.png)' },
+    { id: 'c4', name: 'Teal Pulse', rarity: 'comum', price: 0, image: 'url(/assets/cards/c4.png)' },
     { id: 'c5', name: 'Indigo Beat', rarity: 'comum', price: 0, image: 'url(/assets/cards/common.png)' },
     { id: 'c6', name: 'Carbon Note', rarity: 'comum', price: 0, image: 'url(/assets/cards/common.png)' },
 
@@ -20,7 +20,7 @@ export const CARDS: Card[] = [
 
     // ÉPICO - 30,000 XP
     { id: 'e1', name: 'Molten Gold', rarity: 'épico', price: 0, image: 'url(/assets/cards/epic.png)' },
-    { id: 'e2', name: 'Crimson Storm', rarity: 'épico', price: 0, price_val: 0, image: 'url(/assets/cards/epic.png)' },
+    { id: 'e2', name: 'Crimson Storm', rarity: 'épico', price: 0, image: 'url(/assets/cards/epic.png)' },
     { id: 'e3', name: 'Deep Nebula', rarity: 'épico', price: 0, image: 'url(/assets/cards/epic.png)' },
     { id: 'e4', name: 'Abyssal Key', rarity: 'épico', price: 0, image: 'url(/assets/cards/epic.png)' },
 
